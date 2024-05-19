c86cb96ec2f9d3c53bf6f653abc7ea931da4c1c2
Saito-3815 <sai.engineer3815@gmail.com>
2024-05-20 00:05:43 +0900

    finish

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+      extend: {
+        colors: { //カスタマイズカラーの設定
+          body: "#050505",
+          "selected-text": "#A3A3FF",
+          theme: "#5c318c",
+          secondary: "#9191A4",
+          badge: "#3F3F51",
+          inputBorder: "#565666",
+          input: "#2A2A33",
+        }
+      },
+    fontFamily:{ //フォントの設定（インポートが必要）
+      poppins:["Poppins", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
 
